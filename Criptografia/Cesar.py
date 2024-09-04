695a687c86d1e90c1c7001cf118712b83c7d8b26
David Vigil <54321849+DavidVigil@users.noreply.github.com>
2024-09-03 22:51:50 -0600

    Update Cesar.py
    I finished commenting the code and now it is completely understandable and I am able to continue working on it in the future

--- Criptografia/Cesar.py
+++ Criptografia/Cesar.py
@@ -62,31 +62,31 @@ def cesarIsometricoCifrar(): #Esta función cifra un texto claro pidiendo una cl
     mensaje = []
     texto = input("Introduce el texto a cifrar: ")
     clave = input("Introduce la clave: ") 
-    if len(clave) < len(texto):
+    if len(clave) < len(texto): #Esta estructura se encarga de propagar la clave para que cubra en su totalidad la extensión del mensaje
         for i in range(len(texto)):
             if clave[i%len(clave)] != " ":
                 llave.append(clave[i%len(clave)].upper())
             else:
                 continue
         for letra in texto:
-            if letra != " ":
+            if letra != " ": #Los espacios se omiten al cifrar
                 mensaje.append(letra.upper())
             else:
                 continue
-        for i in range(len(mensaje)):
+        for i in range(len(mensaje)):  #realiza la suma de posiciones del mensaje claro y de la clave, y realiza el módulo para obtener el nuevo caracter
             result += ABC[(ABC.index(mensaje[i])+ABC.index(llave[i]))%len(ABC)]
         print("Criptograma: ", result)
         print("Clave: ", llave)
-    elif len(clave) > len(texto):
+    elif len(clave) > len(texto): #No se admite una clave más grande que el texto
         print("La clave es más larga que el mensaje. Intenta con una clave de igual o menor tamaño")
-        cesarIsometricoCifrar()
+        cesarIsometricoCifrar() #Solicitamos la clave nuevamente utilizando recursividad
     else:
         for letra in texto:
             mensaje.append(letra.upper())
         for letra in clave:
             llave.append(letra.upper())
         for i in range(len(mensaje)):
-            result += ABC[(ABC.index(mensaje[i])+ABC.index(llave[i]))%len(ABC)]
+            result += ABC[(ABC.index(mensaje[i])+ABC.index(llave[i]))%len(ABC)] #Realiza el cifrado
         print("Criptograma: ", result)
         print("Clave: ", llave)
     return clave
@@ -95,9 +95,9 @@ def cesarIsometricoDescifrar(clave):
     result = ""
     llave = []
     mensaje = []
-    texto = input("Introduce el texto a descifrar: ")
+    texto = input("Introduce el texto a descifrar: ") #Recibimos el criptograma
     if len(texto) < len(clave):
-        print("El mensaje es más corto que la clave. Intenta con un mensaje de igual o mayor tamaño")
+        print("El mensaje es más corto que la clave. Intenta con un mensaje de igual o mayor tamaño") #No se admiten criptogramas más cortos que la clave
         cesarIsometricoDescifrar(clave)
     else:
         for i in range(len(texto)):
@@ -108,13 +108,15 @@ def cesarIsometricoDescifrar(clave):
             else:
                 continue
         for i in range(len(mensaje)):
-            result += ABC[(ABC.index(mensaje[i])-ABC.index(llave[i]))%len(ABC)]
+            result += ABC[(ABC.index(mensaje[i])-ABC.index(llave[i]))%len(ABC)] #Descifra el criptograma sumando las posiciones y realizando el módulo
     print("Mensaje claro: ", result)
 
 Abc = [] # Abecedario en el que se pone la clave
-ABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"," "]
-abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
+ABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"," "] #Abecedario con números y espacio
+abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"] #Abecedario de minúsculas. Puede omitirse si los resultados se entregan solo en mayúsculas
 
+#----- MAIN -----
+#Se solicitan los datos y se muestra al momento el resultado
 print("Cifrado y descifrado César")
 texto = input("Introduce el texto a cifrar: ")
 print("Criptograma: ", cesarCifrar(texto))
